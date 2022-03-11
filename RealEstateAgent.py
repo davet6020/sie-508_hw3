8cf5d1761372e309318526c752987f4f0f85350a
davet6020 <davet6020@gmail.com>
2022-03-11 15:43:54 -0500

    Now I can print out the list of properties after assigning them to realtors

--- RealEstateAgent.py
+++ RealEstateAgent.py
@@ -4,20 +4,27 @@ class RealEstateAgent:
     property_list = {}
 
     def __init__(self):
-        self.properti = None
-        self.propertiid = None
         self.agent = None
+        self.propertiid = None
+        self.address = None
+        self.property_type = None
+        self.rent = None
         self.none = None
 
     def listproperties(self):
         self.none = None
-        for p_id, p_info in RealEstateAgent.property_list.items():
-            print('Agent: {} --- Property: {}'.format(p_info['agent'], p_info['properti']))
+        print("Agent -\tRent -\tProperty Type -\tAddress")
+        for pid, pinfo in RealEstateAgent.property_list.items():
+            print("{: >10} {: >6} {: >15} {: >30}".format(pinfo['agent'], pinfo['rent'], pinfo['property_type'],
+                                                          pinfo['address']))
 
-    def addproperty(self, agent, properti, propertiid):
+    def addproperty(self, agent, propertiid,  address, property_type, rent):
         self.agent = agent
-        self.properti = properti
         self.propertiid = propertiid
+        self.address = address
+        self.property_type = property_type
+        self.rent = rent
 
-        RealEstateAgent.pr = {'agent': self.agent, 'properti': self.properti}
+        RealEstateAgent.pr = {'agent': self.agent, 'address': self.address,
+                              'property_type': self.property_type, 'rent': self.rent}
         RealEstateAgent.property_list[self.propertiid] = RealEstateAgent.pr
--- main.py
+++ main.py
@@ -2,34 +2,41 @@ from RealEstateAgent import RealEstateAgent
 from Property import Property
 from House import House
 from Apartment import Apartment
+import random
 
-property_count = 1
-pnum = 1
-pnam = "p"
-rnam = "r"
+mls = {}
+property_count = 0
 
 agent1 = RealEstateAgent()
 agent2 = RealEstateAgent()
 
 print("Input Three Properties")
-while property_count < 4:
-    propx = {pnum: pnam+str(pnum)}
-    realx = {pnum: rnam+str(pnum)}
+while property_count < 3:
+    p = Property()
 
-    propx[pnum] = Property()
+    for attr, value in p.__dict__.items():
+        if attr == 'address':
+            mls['address'] = value
+        if attr == 'rent':
+            mls['rent'] = value
 
     AorH = input("Is this an Apartment or a House (A or H): ")
     if AorH.upper() == "A":
         a = Apartment()
+        mls['property_type'] = 'Apartment'
     else:
         h = House()
+        mls['property_type'] = 'House'
+
+    # Time to assign property to realtor
+    agent1.addproperty(random.choice(RealEstateAgent.agents), property_count, mls['address'], mls['property_type'], mls['rent'])
 
     property_count += 1
-    pnum += 1
 
-# Time to assign property to realtor
 
-agent1.addproperty('Jewel', 'House 1', 0)
-agent1.addproperty('Jewel', 'Apartment 1', 1)
-agent2.addproperty('Tom', 'House 2', 2)
+# agent1.addproperty('Jewel', 'Apartment 1', 1)
+# agent2.addproperty('Tom', 'House 2', 2)
+print("===============================")
+agent1.listproperties()
+print("===============================")
 agent2.listproperties()
